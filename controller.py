b8d468687c804fbf22854e6bd8bfad47c169da7f
Kenso Trabing <ktrabing@acm.org>
2019-01-23 21:35:52 -0500

    Dump

--- controller.py
+++ controller.py
@@ -12,21 +12,29 @@ def game_loop(meeting_name,meeting_duration):
     # FIXME Check for the following conditions:
     #       - `meeting_duration` >= 1
     #       - `meeting_name`
-    # TODO  
-    counter = 0
-    while True:
-        time.sleep(1)
-        # TODO This should have conditions to evaluate against checks in:
-        #      - `halftime`
-        #      - `warning_10`
-        #      - `warning_1`
-        #      - `warning_0` (i.e., `publish_message`)
-        if counter == meeting_duration:
-            return 'times up'
-        elif counter == (meeting_duration-60):
-            print('1 minute left')
-        else:
-            counter += 1
+    if isinstance(meeting_duration,'int'):
+        
+	    counter = 0
+	    while True:
+	        time.sleep(1)
+	        # TODO This should have conditions to evaluate against checks in:
+	        #      - `halftime`
+	        #      - `warning_10`
+	        #      - `warning_1`
+	        #      - `warning_0` (i.e., `publish_message`)
+	        if counter == meeting_duration: # max
+	            return 'times up'
+	        elif counter == (meeting_duration-60): # max - 60
+	            counter += 1
+
+	            print('1 minute left')
+
+	        elif counter == (meeting_duration//2): # max - 60
+	            print('halftime')
+	            counter += 1
+	            
+	        else:
+	            counter += 1
 
 
 if __name__ == '__main__':
