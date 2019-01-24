4a801733077aa75fcc5cef0b2065ac348b0a2a60
Kenso Trabing <ktrabing@acm.org>
2019-01-23 20:24:53 -0500

    Update runtime

--- controller.py
+++ controller.py
@@ -22,3 +22,8 @@ if __name__ == '__main__':
     meeting_name     = str(input('What\'s the name of your meeting? '))
     meeting_duration = int(input('How long is your meeting going to be? '))
     print(game_loop(meeting_name,meeting_duration))
+    print(game_loop(None,None))
+    print(game_loop(0,0))
+    print(game_loop('a','a'))
+    print(game_loop(1.,1.))
+    print(game_loop(2,2))
