8359889d5610b2186b014cb133500092242d66df
Kenso Trabing <ktrabing@acm.org>
2019-01-23 20:22:38 -0500

    Dump

--- controller.py
+++ controller.py
@@ -3,19 +3,22 @@
 import os
 import time
 
+#def check_time():
+#    
 
 def game_loop(meeting_name,meeting_duration):
     counter = 0
     while True:
         time.sleep(1)
-        counter += 1
         # FIXME This should have conditions to evaluate against checks in:
         #       - halftime, warning_*
         if counter == meeting_duration:
             return 'times up'
+        else:
+            counter += 1
 
 
 if __name__ == '__main__':
-    meeting_name     = input('What\'s the name of your meeting? ')
-    meeting_duration = input('How long is your meeting going to be? ')
+    meeting_name     = str(input('What\'s the name of your meeting? '))
+    meeting_duration = int(input('How long is your meeting going to be? '))
     print(game_loop(meeting_name,meeting_duration))
